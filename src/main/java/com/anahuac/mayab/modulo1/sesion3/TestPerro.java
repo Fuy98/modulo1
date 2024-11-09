9e7ef9839140e06f5baa01a0b3683d4744bc8a44
Fuy98 <79111629+Fuy98@users.noreply.github.com>
2024-11-08 21:59:55 -0600

    Archivos sesion 3

---
+++ src/main/java/com/anahuac/mayab/modulo1/sesion3/TestPerro.java
@@ -0,0 +1,28 @@
+package com.anahuac.mayab.modulo1.sesion3;
+
+public class TestPerro {
+	
+	public static void main(String[] args) {
+		
+		Perro perro1 = new Perro();
+		
+		Perro perro2 = new Perro("desconocida", "pequeño", 10.5,"firulais", "pepe",2);
+		
+		System.out.println("El perro1 se llama: " + perro1.getNombre());
+		
+		System.out.println("El perro2 se llama: " + perro2.getNombre());
+		
+		
+		//Ya sabemos el nombre de perro1 
+		
+		perro1.setNombre("solovino");
+		
+		System.out.println("El perro1 se llama: " + perro1.getNombre());
+		
+		System.out.println("el perro2 ladra: ");
+		perro2.ladrar();
+		
+	}
+	
+
+}
